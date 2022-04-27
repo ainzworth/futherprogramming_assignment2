704577b888af37d3f624b3b7d51db6527cb5e984
ainzworth <s3826227@rmit.edu.vn>
2022-04-27 10:45:06 +0700

    entity fixed

--- src/main/java/com/example/assignment2/Assignment2Application.java
+++ src/main/java/com/example/assignment2/Assignment2Application.java
@@ -1,18 +1,19 @@
 package com.example.assignment2;
 
-import com.example.assignment2.appConfig.AppConfig;
-import com.example.assignment2.services.CarService;
-import com.example.assignment2.services.DriverService;
+import appConfig.AppConfig;
+import services.CarService;
+import services.DriverService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 		CarService carService1 = context.getBean(CarService.class);
 		DriverService driverService1 = context.getBean(DriverService.class);
