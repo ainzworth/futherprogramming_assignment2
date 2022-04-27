c98ccc6019401c68d755356cb3221bc2c0dad933
ainzworth <s3826227@rmit.edu.vn>
2022-04-27 10:51:16 +0700

    change file path

---
+++ src/main/java/appConfig/AppConfig.java
@@ -0,0 +1,75 @@
+package appConfig;
+
+import entity.*;
+import services.CarService;
+import services.DriverService;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+    @Bean
+    public Car car(){
+        return new Car();
+    }
+    @Bean
+    public Booking booking(){
+        return new Booking();
+    }
+    @Bean
+    public Customer customer(){
+        return new Customer();
+    }
+    @Bean
+    public Invoice invoice(){
+        return new Invoice();
+    }
+    @Bean
+    public Driver driver(){
+        return new Driver();
+    }
+
+    // transactionManagement
+
+    @Bean
+    public DriverService driverService(){
+        return new DriverService();
+    }
+    @Bean
+    public CarService carService(){
+        return new CarService();
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+        HibernateTransactionManager tx = new HibernateTransactionManager(sessionFactory);
+        return tx;
+    }
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(){
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.show_sql", true);
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //To use postgresql
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/demo");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("123");
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setHibernateProperties(properties);
+        sessionFactoryBean.setPackagesToScan("entity");
+        return sessionFactoryBean;
+    }
+
+}
---
+++ src/main/java/services/CarService.java
@@ -0,0 +1,26 @@
+package services;
+
+import entity.Car;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public class CarService {
+    @Autowired
+    private SessionFactory sessionFactory;
+    public CarService(){}
+    public CarService(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void saveCar(Car car){
+        sessionFactory.getCurrentSession().save(car);
+    }
+    //hello
+}
